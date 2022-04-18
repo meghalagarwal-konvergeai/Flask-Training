bbf6f4611332372ad83bf1142d4e2a1e402cc179
Meghal Agarwal <meghal.agarwal@konverge.ai>
2022-04-18 21:36:13 +0530

    Created a Home page to store ToDo List

--- Basic/app.py
+++ Basic/app.py
@@ -1,5 +1,5 @@
 from turtle import title
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -11,16 +11,22 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    description = db.Column(db.String(500), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f"{self.sno} - {self.title}"
 
-@app.route("/")
-@app.route("/home")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('index.html')
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html', alltodo=allTodo)
 
 if __name__ == "__main__":
     app.run(debug=True)
