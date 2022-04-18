c4fd14040d0b41c2312a829835e888946ff42be5
Meghal Agarwal <meghal.agarwal@konverge.ai>
2022-04-18 19:48:34 +0530

    Created Database

--- Basic/app.py
+++ Basic/app.py
@@ -1,6 +1,22 @@
+from turtle import title
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///mytodo.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+
 @app.route("/")
 @app.route("/home")
 def home():
